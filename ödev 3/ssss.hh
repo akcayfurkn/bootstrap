0f7d65911a816c23c27e46e697aaedcdf6fe6a85
akcayfurkn <113826704+akcayfurkn@users.noreply.github.com>
2022-10-23 14:32:23 +0300

    Add files via upload

---
+++ ödev 3/ssss.hh
@@ -0,0 +1,17 @@
+    <button
+                class="navbar-toggler "
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              ></button>
+              <form class="form-inline">
+                <input
+                  class="form-control searche"
+                  type="search"
+                  placeholder="Arama Yap"
+                  aria-label="Search"
+                />
+          </form>
